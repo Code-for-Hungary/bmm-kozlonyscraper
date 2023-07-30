78a2cedb2783b68f91300a7fdca5e4971aa51b6e
law909 <balint.lovey@gmail.com>
2023-07-30 04:17:43 +0200

    fts

--- bmmtools.py
+++ bmmtools.py
@@ -3,7 +3,7 @@ import re
 def searchstringtofts(searchstring):    
     keresoszo = searchstring.strip()
     keresoszo = re.sub(r'\s+', ' ', keresoszo)
-    keresoszo = re.sub(r'([()])', '', keresoszo)
+    keresoszo = re.sub(r'([()\-])', '', keresoszo)
     if keresoszo:
         if not re.search(r'(["+*])', keresoszo):
             keresoszo = re.sub(r'([\s])', ' + ', keresoszo) + '*'
