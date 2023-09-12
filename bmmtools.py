723f56c3d3953035e3c6ef209c631d14d53f7d96
law909 <balint.lovey@gmail.com>
2023-09-12 22:39:22 +0200

    empty searcstring

--- bmmtools.py
+++ bmmtools.py
@@ -1,12 +1,13 @@
 import re
 
-def searchstringtofts(searchstring):    
-    keresoszo = searchstring.strip()
-    keresoszo = re.sub(r'\s+', ' ', keresoszo)
-    keresoszo = re.sub(r'([()\-])', '', keresoszo)
-    if keresoszo:
-        if not re.search(r'(["+*])', keresoszo):
-            keresoszo = re.sub(r'([\s])', ' + ', keresoszo) + '*'
+def searchstringtofts(searchstring):
+    if isinstance(searchstring, str):
+        keresoszo = searchstring.strip()
+        keresoszo = re.sub(r'\s+', ' ', keresoszo)
+        keresoszo = re.sub(r'([()\-])', '', keresoszo)
+        if keresoszo:
+            if not re.search(r'(["+*])', keresoszo):
+                keresoszo = re.sub(r'([\s])', ' + ', keresoszo) + '*'
 
     return keresoszo
 
